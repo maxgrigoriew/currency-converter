824ff686e11422f7b065a55d3f735b58aac2c3a9
Max <sss123123111@yandex.ru>
2022-12-05 00:12:17 +0300

    fixed table and nominal

--- src/pages/CurrencyPage.vue
+++ src/pages/CurrencyPage.vue
@@ -2,11 +2,12 @@
    <v-col md="12" lg="9" xl="6" class="mx-auto">
       <h1 class="text-center mb-4">Курс валют на сегодня</h1>
       <v-row>
-         <v-col cols="6">Выберите валюту: {{ selected }}</v-col>
+         <v-col cols="6">Базовая валюта: {{ defaultValute.Name }}</v-col>
          <v-col>
             <v-select v-model="selected[0]"
                       :items="countries"
                       item-text="Name"
+                      label="Выберите валюту"
                       item-value="CharCode">
             
             </v-select>
@@ -14,10 +15,16 @@
       </v-row>
       <v-row>
          <v-col>
-            <v-simple-table>
+            <v-simple-table class="table">
                <template>
                   <thead>
                   <tr>
+                     <th class="text-left">
+                        Код
+                     </th>
+                     <th class="text-left">
+                        Номинал
+                     </th>
                      <th class="text-left">
                         Валюта
                      </th>
@@ -25,23 +32,25 @@
                         Курс
                      </th>
                      <th class="text-left">
-                        Предудущее значение
+                        Изменение
                      </th>
                   </tr>
                   </thead>
                   <tbody>
                   <tr v-for="valute in valutes" :key="valute.name">
+                     <td>{{ valute.CharCode }}</td>
+                     <td>{{ valute. Nominal}}</td>
                      <td>{{ valute.Name }}</td>
                      <td>{{ valute.Value }}</td>
                      <td>
-                        {{ valute.Previous }}
+<!--                        {{ valute.Previous }}-->
                         <span v-if="valute.Value > valute.Previous" style="color: green"
                         >▲</span
                         >
                         <span
                             v-else-if="valute.Value === valute.Previous"
                             style="color: black"
-                        >▲▼</span
+                        ></span
                         >
                         <span v-else style="color:red">▼</span>
                      </td>
@@ -65,11 +74,7 @@ export default {
          valutes: {},
          copyValutes: {},
          selected: ["USD"],
-         inputed: "",
-         result: null,
-         countries: [
-           
-         ],
+         countries: [],
          defaultValute: {},
       }
    },
@@ -80,49 +85,40 @@ export default {
              .then((response) => {
                 this.copyValutes = JSON.parse(JSON.stringify(response.data.Valute))
                 this.valutes = JSON.parse(JSON.stringify(this.copyValutes))
-                this.defaultValute = {...this.valutes["EUR"]}
-                for (let valute in this.valutes) {
-                   let object = this.valutes[valute]
-                   
-                   let secondValuteValue = object.Value
-                   let firstValuteValue = this.defaultValute.Value
-                   let newValuteValue = firstValuteValue / secondValuteValue
-                   
-                   let secondValutePrevious = object.Previous
-                   let firstValutePrevious = this.defaultValute.Previous
-                   let newValutePrevios = firstValutePrevious / secondValutePrevious
-                   this.valutes[valute].Value = newValuteValue.toFixed(4)
-                   this.valutes[valute].Previous = newValutePrevios.toFixed(4)
-                }
+                this.defaultValute = this.valutes["USD"]
+                this.calculateCurency()
                 this.countries = Object.values(response.data.Valute).map((obj) => {
                   return {
                      Name: obj.Name,
                      CharCode: obj.CharCode
                   }
                 })
-                console.log(this.countries)
              })
              .catch((error) => {
                 console.log(error)
              })
       },
-   },
-   watch: {
-      selected(one, two) {
-         console.log(one, two)
+      calculateCurency() {
          let copyValutes = JSON.parse(JSON.stringify(this.copyValutes))
          this.defaultValute = Object.values(this.copyValutes).find((obj) => obj.CharCode == this.selected)
          for (let valute in copyValutes) {
             let secondValuteValue = copyValutes[valute].Value
             let firstValuteValue = this.defaultValute.Value
-            let newValuteValue = firstValuteValue / secondValuteValue
-            
-            let secondValutePrevious = copyValutes[valute].Previous
+            let firstValuteNominal = copyValutes[valute].Nominal
+            let secondValuteNominal = copyValutes[valute].Nominal
             let firstValutePrevious = this.defaultValute.Previous
+            let secondValutePrevious = copyValutes[valute].Previous
+            let newValuteValue = firstValuteValue / firstValuteNominal / (secondValuteValue / secondValuteNominal)
             let newValutePrevios = firstValutePrevious / secondValutePrevious
             this.valutes[valute].Value = newValuteValue.toFixed(4)
             this.valutes[valute].Previous = newValutePrevios.toFixed(4)
          }
+         console.log(this.defaultValute)
+      }
+   },
+   watch: {
+      selected() {
+         this.calculateCurency()
       }
    },
    mounted() {
@@ -131,4 +127,9 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.table {
+   max-height: 500px;
+   overflow-y: scroll;
+}
+</style>
